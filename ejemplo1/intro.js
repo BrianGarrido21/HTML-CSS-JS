71cf8a0ee320d940ea3ee7e6387cdb935644b286
BrianGarrido21 <briangarridopicon@gmail.com>
2023-03-23 19:59:10 +0100

    Ejercicios JavaScript

---
+++ ejemplo1/intro.js
@@ -0,0 +1,1 @@
+alert("Mensaje creado en el archivo...")
---
+++ ejemplo4/ejemplo4.js
@@ -0,0 +1,16 @@
+var edad = 19.1234;
+var numero = "12";
+var nombre = "Sonia";
+var apellido = "Martin";
+var alumnos=["Mortadelo","Filemon","zipi","zape"];
+var texto = "Los mejores alumnos";
+var objeto={
+    color:"negro",
+    size:12
+};
+var booleano=true;
+var iVariable;
+var numero2=10;
+var numbero2=0;
+var numero2=null;
+var texto2="Hola como estas?";
